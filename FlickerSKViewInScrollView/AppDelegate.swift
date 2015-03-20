bafcbe0d67e60ac43bfbe98bc49dced34b605b31
daniel j farrell <daniel.james.farrell@gmail.com>
2015-03-20 10:50:24 +0000

    Initial Commit

---
+++ FlickerSKViewInScrollView/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  FlickerSKViewInScrollView
+//
+//  Created by Daniel Farrell on 20/03/2015.
+//  Copyright (c) 2015 Daniel Farrell. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
